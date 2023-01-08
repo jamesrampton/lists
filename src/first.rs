133b5619b05451be929bce008914ca7b1c541e2e
James Rampton <me@jamesrampton.com>
2023-01-08 09:30:45 +0000

    Add some tests

--- src/first.rs
+++ src/first.rs
@@ -37,3 +37,37 @@ impl List {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves correctly
+        assert_eq!(list.pop(), None);
+
+        // Populate the list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to be sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
